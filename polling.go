41bf2287e278f690c2054cba50b37b1619443807
yogisec <yogisec7@gmail.com>
2020-11-08 09:35:35 -0600

    polling

--- polling.go
+++ polling.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-  "fmt"
-  "time"
-  "net/http"
+	"fmt"
+	"net/http"
+	"time"
 )
 
 func doSomething(s string) {
-  fmt.Println("doing something", s)
+	fmt.Println("doing something", s)
 }
 
 func startPolling1() {
-  for {
-    time.Sleep(2 * time.Second)
-    go doSomething("from polling 1")
-  }
+	for {
+		time.Sleep(2 * time.Second)
+		go doSomething("from polling 1")
+	}
 }
 
 func startPolling2() {
-  for {
-    <-time.After(2 * time.Second)
-    go doSomething("from polling 2")
-  }
+	for {
+		<-time.After(4 * time.Second)
+		go doSomething("from polling 2")
+	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-  go startPolling1()
-  go startPolling2()
+	go startPolling1()
+	go startPolling2()
 
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
