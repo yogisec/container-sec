d0f92f4e60f5728a2a32112ba0ad0dd80ebfee06
yogisec <yogisec7@gmail.com>
2020-11-08 09:16:00 -0600

    Host JSON Payload

--- agent.go
+++ agent.go
@@ -29,8 +29,8 @@ import (
 */
 
 type fullContainerDetails struct {
-	ContainerID      string          `json:"ContainerID"`
-	ContainerDetails L.ContainerData `json:"ContainerDetails"`
+	ContainerID      string           `json:"ContainerID"`
+	ContainerDetails *L.ContainerData `json:"ContainerDetails"`
 }
 
 func getMacAddr() ([]string, error) {
